bd5437b268f3799cb8c57b913d9aa6d368e548a1
Lucas Soro <lsoro@fi.uba.ar>
2026-04-03 03:43:06 -0300

    Basic exchange implementation

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -3,8 +3,7 @@ import time
 import pika
 from pika.adapters.blocking_connection import BlockingChannel
 from pika.spec import Basic
-import random
-import string
+from pika.frame import Method
 from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
@@ -41,6 +40,43 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
         self.connection.close()
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
+
+    def _on_message_received(self, ch: BlockingChannel, method: Basic.Deliver, properties, body: bytes):
+        ack_func = lambda: ch.basic_ack(delivery_tag = method.delivery_tag)
+        self.on_message_callback(body, ack_func, ch.basic_nack)
     
     def __init__(self, host, exchange_name, routing_keys):
-        pass
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+        self.channel = self.connection.channel()
+        self.exchange_name = exchange_name
+        self.routing_keys = routing_keys
+
+        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='direct')
+
+    def start_consuming(self, on_message_callback):
+        result: Method = self.channel.queue_declare(queue='', exclusive=True)
+        queue_name: str = result.method.queue
+
+        for key in self.routing_keys:
+            self.channel.queue_bind(exchange=self.exchange_name,
+                                    queue=queue_name,
+                                    routing_key=key)
+        
+        self.on_message_callback = on_message_callback
+        self.channel.basic_consume(queue=queue_name, 
+                                   on_message_callback=self._on_message_received, 
+                                   auto_ack=False)
+        self.channel.start_consuming()
+
+    def stop_consuming(self):
+        self.channel.stop_consuming()
+
+    def send(self, message):
+        # Should publishers with multiple routing keys be supported?
+        for key in self.routing_keys:
+            self.channel.basic_publish(exchange=self.exchange_name,
+                                       routing_key=key,
+                                       body=message)
+
+    def close(self):
+        self.connection.close()
